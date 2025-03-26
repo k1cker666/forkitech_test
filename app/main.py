c3c3530b68e429b759c982e47b6b842475743cc0
k1cker666 <pomoymu@gmail.com>
2025-03-26 20:06:21 +0300

    feat: add endpoints

--- app/main.py
+++ app/main.py
@@ -1,13 +1,38 @@
-from fastapi import FastAPI
-from schema import TRXAdressSchema
-from models import create_db
+from typing import Annotated, List
+from fastapi import FastAPI, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 
+from db import get_session
+from schema import TRXAdressAddSchema, TRXAdressSchema
+from models import create_db, TRXAdressModel
+
+
+session_dep = Annotated[AsyncSession, Depends(get_session)]
 
 app = FastAPI()
 
-@app.get("/", summary="Пинг ручка")
-def ping():
-    return {"ping": "pong"}
+
+@app.post(
+    "/get_adress_info",
+    summary="Получить информацию по адресу TRX кошелька",
+    description="Эндпоинт возвращает bandwidth, energy, и баланс trx",
+)
+async def get_adress_info(data: TRXAdressAddSchema, session: session_dep):
+    new_trx_adress = TRXAdressModel(trx_adress=data.trx_adress)
+    session.add(new_trx_adress)
+    await session.commit()
+    return {"adress": data.trx_adress}
+
+@app.get(
+    "/get_reqests",
+    summary="Получить список запросов к сервису",
+    description="Эндпоинт возвращает список кошельков, по которым запрашивалась информация",
+)
+async def get_requests(session: session_dep) -> List[TRXAdressSchema]:
+    query = select(TRXAdressModel)
+    result = await session.execute(query)
+    return result.scalars().all()
 
 @app.post("/db")
 async def db() -> None:
--- app/schema.py
+++ app/schema.py
@@ -1,5 +1,8 @@
 from pydantic import BaseModel, Field
 
 
-class TRXAdressSchema(BaseModel):
+class TRXAdressAddSchema(BaseModel):
     trx_adress: str = Field(min_length=34, max_length=34, pattern=r"^T[A-Za-z1-9]{33}$")
+
+class TRXAdressSchema(TRXAdressAddSchema):
+    id: int
