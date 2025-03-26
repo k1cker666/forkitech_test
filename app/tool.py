823be8237b3efc11babdbf11ab4107a66d872cf6
k1cker666 <pomoymu@gmail.com>
2025-03-27 00:03:18 +0300

    feat: add tool for recreate tables

--- app/tool.py
+++ app/tool.py
@@ -3,11 +3,11 @@ import asyncio
 from deps import engine
 from models import Base
 
-async def create_db():
+async def create_tables():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
 
 if __name__ == "__main__":
-    asyncio.run(create_db())
+    asyncio.run(create_tables())
